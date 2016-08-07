c76ad23afecabac3b6baaf1f76ea43cf3c0432c8
shdhumale@gmail.com <shdhumale@gmail.com>
2016-08-07 10:14:36 +0530

    first commit

---
+++ app/src/main/java/siddhu/test/com/myreceiver/MyReceiver.java
@@ -0,0 +1,16 @@
+package siddhu.test.com.myreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by admin on 8/7/2016.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
